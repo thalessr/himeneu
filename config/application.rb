8a39bde9b227af9fed252f713ac04218110fc0ef
Thales Ribeiro <thalessr@gmail.com>
2014-12-03 16:29:05 -0200

    Setting a default locale to pt-BR

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Himeneu
   class Application < Rails::Application
-    #config.i18n.default_locale = :sv
+    config.i18n.default_locale = "pt-BR"
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
