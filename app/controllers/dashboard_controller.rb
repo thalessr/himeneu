c8339c0a0ee8e59cd0bb0f7dab4eac813266a9c2
Thales Ribeiro <thalessr@gmail.com>
2014-12-05 01:07:44 -0200

    Fixing customers creation

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,11 +2,9 @@ class DashboardController < ApplicationController
 	def index
 		@user = current_user 
 		unless @user.is_completed
-			if @user.role == User::ROLES[1].to_s
-				redirect_to new_customer_path
-			end
+			redirect_to new_customer_path
 		else
-			
+			#redirect users who complted their sign up
 		end
 		
 	end
