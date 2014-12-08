8412f26fa6e87495fb1e37350af523b985a8d744
Thales Ribeiro <thalessr@gmail.com>
2014-12-08 17:27:20 -0200

    Translating rails and showing better errors

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,6 +2,9 @@ class Customer < ActiveRecord::Base
 	belongs_to :user
 	#include all required fields here
 	validates_presence_of :first_name
+	validates_presence_of :last_name
+	validates_presence_of :age
+	validates_presence_of :wedding_date
 
 	def set_completed
 		user = User.find(self.user_id)
--- config/application.rb
+++ config/application.rb
@@ -8,15 +8,16 @@ Bundler.require(*Rails.groups)
 
 module Himeneu
   class Application < Rails::Application
-    config.i18n.default_locale = "pt-BR"
-    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Brasilia'
+    config.i18n.available_locales = [:en, :"pt-BR"]
+    config.i18n.default_locale = :"pt-BR"
+    config.assets.paths << Rails.root.join("vendor","assets", "fonts")
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
