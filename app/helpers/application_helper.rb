71b83209267a9c835c588457a573b40fda6c5688
Thales Ribeiro <thalessr@gmail.com>
2014-12-29 02:12:31 -0600

    It resolves#1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+	def redirect_to_edit
+		if current_user.is_customer?
+		   customer = Customer.find_by_user_id(current_user.id)
+		   if provider
+	           edit_customer_path(customer.id)
+           end
+		else
+		   provider = Provider.find_by_user_id(current_user.id)
+		   if provider
+	           edit_provider_path(provider.id)
+           end
+		end
+	end
 end
