359c4df4f617523eb7950e5bde12edd86e84353b
Thales Ribeiro <thalessr@gmail.com>
2014-12-29 16:24:21 -0600

    Writing better unit tests

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -18,26 +18,32 @@ class ProvidersController < ApplicationController
   end
 
   def edit
-    @provider = Provider.find_by_user_id(current_user.id)
+    @provider = Provider.find(params[:id])
   end
 
   def update
-    @provider = Provider.find_by_user_id(current_user.id)
+    @provider = Provider.find(params[:id])
     if @provider.update_attributes(provider_params)
+      redirect_to @provider
     else
       render :edit
     end
   end
 
   def destroy
+    @provider = Provider.find(params[:id])
+    @provider.destroy
+    redirect_to new_provider_path
   end
 
   def show
-    @provider = Provider.find_by_user_id(current_user.id)
+    @provider = Provider.find(params[:id])
   end
 
   private 
   def provider_params
-    params.require(:provider).permit!
+    params.require(:provider).permit(
+                                      :first_name, :last_name, :age, :contact,
+                                      :image_url, :profession, :city, :experience)
   end
 end
--- test/controllers/providers_controller_test.rb
+++ test/controllers/providers_controller_test.rb
@@ -1,14 +1,19 @@
 require 'test_helper'
+require "mocha/mini_test"
 
 class ProvidersControllerTest < ActionController::TestCase
   fixtures :users
+  fixtures :providers
   include Devise::TestHelpers
 
   def setup
-    @provider = users(:provider_not_completed)
+    @user = users(:bride_not_completed)
+    @provider = providers(:bride)
+    sign_in @user
   end
 
   def teardown
+    @user = nil
     @provider = nil
   end
 
@@ -17,25 +22,34 @@ class ProvidersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get create" do
-    sign_in @provider
-    post :create
-    assert_response :success
-  end
-
   test "should get new" do
     get :new
     assert_response :success
   end
 
-  test "should get destroy" do
-    sign_in @provider
-    delete :destroy, id: @provider
+  test "should create a provider" do
+    assert_difference('Provider.count') do
+      post :create,provider: @provider.attributes
+    end
+    assert_redirected_to assigns(:provider)
+  end
+
+  test "should get edit" do
+    get :edit, id: @provider
     assert_response :success
   end
 
+  test "should update a provider" do
+    put :update, id: @provider ,provider: @provider.attributes
+    assert_redirected_to assigns(:provider)
+  end
+
+  test "should get destroy" do
+    delete :destroy, id: @provider
+    assert_redirected_to(controller: "providers", action: "new")
+  end
+
   test "should get show" do
-    sign_in @provider
     get :show, id: @provider
     assert_response :success
   end
