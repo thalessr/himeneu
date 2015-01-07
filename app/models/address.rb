f617403aad87583ee3c22de053f866fb60581c29
Thales Ribeiro <thalessr@gmail.com>
2015-01-07 14:48:20 -0600

    Put geocoder to work

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,11 @@
 class Address < ActiveRecord::Base
 	has_one :customer
+
+	geocoded_by :full_address
+    after_validation :geocode, :if => :city_changed?
+
+    private
+    def full_address
+    	"#{city},#{zipcode}"
+    end
 end
