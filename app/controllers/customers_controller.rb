b89ab3a42cd17018a742f73c984c50eb4a97e960
Thales Ribeiro <thalessr@gmail.com>
2015-01-08 19:24:42 -0600

    Uploading image in a better way

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -6,15 +6,11 @@ class CustomersController < ApplicationController
 
 	def new
 		@customer = Customer.new
-		@uploader = Customer.new.image
 		@customer.build_address
-        # @uploader.success_action_redirect = new_customer_url
 	end
 
 	def create
 		@customer = current_user.customers.build(customer_params)
-		@customer.key = params[:key]
-		 binding.pry
 		if @customer.save
 			current_user.set_completed
 			redirect_to @customer
@@ -26,25 +22,22 @@ class CustomersController < ApplicationController
 	def show
 		@customer = Customer.find(params[:id])
 		@customer.get_wedding_date
-		@uploader = @customer.image
-        @uploader.success_action_redirect = edit_customer_url(@customer.id)
 	end
 
 	def edit
 		@customer = Customer.find(params[:id])
 		@customer.get_wedding_date
-		@customer.update_attribute(:key, params[:key])
 	end
 
 	def update
 		@customer = Customer.find(params[:id])
 
 		if @customer.update_attributes(customer_params)
-			if @customer.image_processed
+			# if @customer.image_processed
 			   redirect_to @customer
-		    else
-			    render :edit
-			end
+		    # else
+			    # render :edit
+			# end
 		else
 			render :edit
 		end
