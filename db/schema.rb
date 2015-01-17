f2fe3bea2c0c9dd272503e8c0908719d0188f0bb
Thales Ribeiro <thalessr@gmail.com>
2015-01-16 23:51:26 -0600

    Adding a phone and email per address

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150113063056) do
+ActiveRecord::Schema.define(version: 20150117054727) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 20150113063056) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "provider_id"
+    t.string   "email"
+    t.string   "phone"
   end
 
   add_index "addresses", ["provider_id"], name: "index_addresses_on_provider_id"
