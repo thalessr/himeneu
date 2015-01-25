52b61cd1a870caef3b8b2ad8b9533752dc5e28d1
Thales Ribeiro <thalessr@gmail.com>
2015-01-25 02:19:23 -0600

    Testing dashboard_controller :gem:

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -2,26 +2,60 @@ require 'test_helper'
 
 class DashboardControllerTest < ActionController::TestCase
 	fixtures :users
+	fixtures :providers
+	fixtures :roles
 	include Devise::TestHelpers
+
+
+	def set_ability(user)
+		@ability = Ability.new(user)
+    @ability.extend(CanCan::Ability)
+    @controller.stubs(:current_ability).returns(@ability)
+	end
 
 	test "Should get index" do
 		@user = users(:bride_completed)
-  	    sign_in @user
+  	sign_in @user
 		get :index
-		assert_response :success
+		assert_redirected_to(controller: "registrations", action: "new")
 	end
 
-	test "Bride Should get new customers path" do
-		@user = users(:bride_not_completed)
-  	    sign_in @user
+	test "Not completed customer should get the new customers path" do
+	  @user = users(:bride_not_completed)
+	  @user.roles << roles(:noiva)
+		set_ability(@user)
+  	sign_in @user
+  	@ability.can :create, Customer
 		get :index
 		assert_redirected_to(controller: "customers", action: "new")
 	end
 
-	test "Provider should get new providers path" do
-		@user = users(:provider_not_completed)
-  	    sign_in @user
+	test "Completed customer should get the providers path" do
+	  @user = users(:bride_completed)
+	  @user.roles << roles(:noiva)
+		set_ability(@user)
+  	sign_in @user
+		get :index
+		assert_redirected_to(controller: "providers", action: "index")
+	end
+
+	test "Not completed provider should get the new providers path" do
+	  @user = users(:provider_not_completed)
+	  @user.roles << roles(:fornecedor)
+		set_ability(@user)
+  	sign_in @user
 		get :index
 		assert_redirected_to(controller: "providers", action: "new")
+	end
+
+	test "Completed provider should get the root path" do
+	  @user = users(:provider_completed)
+	  @user.roles << roles(:fornecedor)
+	  @user.provider = providers(:bride)
+		set_ability(@user)
+  	sign_in @user
+		get :index
+		assert_response :success
+		assert_template :index
 	end
 end
