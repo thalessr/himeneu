3b351a75d5bba2feeb391802cddb86758044a002
Thales Ribeiro <thalessr@gmail.com>
2015-01-26 01:20:57 -0600

    Not uploading images to S3 when in development env

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,40 +4,29 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWaveDirect::Uploader
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  include ::CarrierWave::Backgrounder::Delay
   include CarrierWave::MiniMagick
-
-  # Choose what kind of storage to use for this uploader:
-  #storage :file
-  storage :fog
   include CarrierWave::MimeTypes
+
   process :set_content_type
-  # enable_processing = true
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "public/uploads/tmp"
+  if Rails.env.production?
+    include ::CarrierWave::Backgrounder::Delay
+    storage :fog
+
+    def store_dir
+      "public/uploads/tmp"
+    end
+
+    def cache_dir
+      "#{Rails.root}/tmp/uploads"
+    end
+  else
+    storage :file
+
+    def store_dir
+      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
   end
-
-  def cache_dir
-    "#{Rails.root}/tmp/uploads"
-  end
-
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
-
-  # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
 
   # Create different versions of your uploaded files:
   version :thumb do
