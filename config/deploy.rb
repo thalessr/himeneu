12719f392b6f5d674aacb20cc580c154466e8105
Thales Ribeiro <thalessr@gmail.com>
2015-01-29 21:24:54 -0600

    Env variables problem

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,6 +31,7 @@ set :pty, true
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+# set :linked_files, %w{config/database.yml config/secrets.yml .env}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
@@ -40,6 +41,7 @@ set :pty, true
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
 
 namespace :deploy do
 
