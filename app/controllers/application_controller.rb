5a4cfe74c0f64c75734b2fd4a92af93385d88ec4
Thales Ribeiro <thalessr@gmail.com>
2015-02-01 14:08:35 -0600

    Tested this solution on my machine running in production mode, it worked.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :current_ability
+  before_filter :set_mailer_host
   before_action :set_locale #, :authenticate_user!
 
 
@@ -18,4 +19,9 @@ class ApplicationController < ActionController::Base
   def current_ability
   	@current_ability ||= Ability.new(current_user)
   end
+
+  def set_mailer_host
+    ActionMailer::Base.default_url_options[:host] = "himeneu.com"
+  end
+
 end
