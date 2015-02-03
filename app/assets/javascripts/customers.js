c99b066e0b8ea5ba0c8f9f35b5ca320e08d29237
Thales Ribeiro <thalessr@gmail.com>
2015-02-02 20:27:10 -0600

    What I have work on my local machine

--- app/assets/javascripts/customers.js
+++ app/assets/javascripts/customers.js
@@ -1,6 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $( document ).ready(function() {
+  'use strict';
 	$(function () {
 
        $("[data-behaviour~='datepicker']").focus(function() {
