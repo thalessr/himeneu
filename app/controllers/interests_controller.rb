3082e4d16c99b6f4db204cc10d1f0f77a878a104
Rodrigo Vitulli <rovitulli@gmail.com>
2015-02-05 13:13:55 -0200

    fixing a typo, adding button with post and saving interest V1

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -2,11 +2,14 @@ class InterestsController < ApplicationController
 
 	def create
 	  @interest = Interest.new(interest_params)
+	  @interest.save
+	  redirect_to provider_path(interest_params[:provider_id])
+	  flash[:notice] = "Aguarde o contato deste prestadore de serviço!"
 	end
 
 	private
   		def interest_params
-    		params.require(:interest).permit(:customer_id, :provider_id)
+    		params.permit(:customer_id, :provider_id)
   		end
 
 end
