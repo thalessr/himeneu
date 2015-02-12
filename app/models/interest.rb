b9b527fd7a963909d90a26c579322c04739f024b
Thales Ribeiro <thalessr@gmail.com>
2015-02-11 19:29:47 -0600

    Showing contact only after click interest

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,4 +1,13 @@
 class Interest < ActiveRecord::Base
 	belongs_to :provider
-  	belongs_to :customer
+  belongs_to :customer
+
+  def self.has_interest?(user, provider)
+    if user.is_customer?
+      customer = Customer.find_by(user_id: user.id)
+      Interest.exists?(customer_id: customer.id , provider_id: provider.id)
+    else
+      false
+    end
+  end
 end
