ce12d872790b5ce750e9a68f1c32ef5680626df4
Thales Ribeiro <thalessr@gmail.com>
2015-02-20 04:57:31 -0600

    Almost done

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -90,7 +90,8 @@ class ProvidersController < ApplicationController
   def provider_params
     params.require(:provider).permit(
                                       :first_name, :last_name, :age,
-                                      :image, :profession_list, :city, :experience, :tag_list,
+                                      :image, :profession_list, :instagram, :website, :twitter,
+                                      :video_url, :facebook,:city, :experience, :tag_list,
                                       addresses_attributes: [:id, :city, :zipcode, :email, :phone, :_destroy]
                                       )
   end
