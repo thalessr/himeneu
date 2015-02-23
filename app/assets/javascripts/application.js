df870e4fb5e745a84b67e5c9d7bb7d0c14d658d4
Thales Ribeiro <thalessr@gmail.com>
2015-02-22 22:19:54 -0600

    Better providers and customers search page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,7 +22,6 @@
 //= require wow/dist/wow.min.js
 //= require bootstrap-datetimepicker
 //= require raty/lib/jquery.raty
-//= require fuelux/dist/js/fuelux.min.js
 //= require bootstrap-notify/js/bootstrap-notify.js
 //= require flat-ui/dist/js/flat-ui.min.js
 //= require videojs/dist/video-js/video.js
