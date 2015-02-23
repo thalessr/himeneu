c0f2a684a8bd06cfb26a47039ae577c682a5db28
Rodrigo Vitulli <rovitulli@gmail.com>
2015-02-23 11:42:32 -0300

    fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,15 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
 
 
-  get 'static_pages/anuncie'
-
-  get 'static_pages/sobre'
-
-  get 'static_pages/contato'
 
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}, :controllers => { registrations: 'registrations', confirmations: 'confirmations'  }
   root to: 'dashboard#index'
   get 'dashboard' => 'dashboard#index'
   get 'privacy' => 'dashboard#privacy'
+  get '/anuncie' => 'static_pages#anuncie'
+  get '/sobre' => 'static_pages#sobre'
+  get '/contato' => 'static_pages#contato'
+
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
