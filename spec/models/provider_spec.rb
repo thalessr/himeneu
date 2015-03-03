878c4a1ac44f88da6a6159c96379ba17e2673afc
Thales Ribeiro <thalessr@gmail.com>
2015-03-02 19:08:22 -0600

    Using factory girl and faker to test

--- spec/models/provider_spec.rb
+++ spec/models/provider_spec.rb
@@ -2,35 +2,37 @@ require 'rails_helper'
 
 describe Provider do
   it "is valid with a first_name and last_name" do
-    provider = Provider.create(first_name: "Alta", last_name: "Floresta")
-    expect(provider).to be_valid
+    expect(build(:provider)).to be_valid
   end
   it "is invalid without a first_name" do
-    provider = Provider.create(first_name: nil, last_name: "Floresta")
+    provider = build(:provider, first_name: nil)
     expect(provider.valid?).to be_falsey
     expect(provider.errors[:first_name].size).to eq(1)
   end
 
   it "returns a provider's full name as a string" do
-   provider = Provider.create(first_name: "Alta", last_name: "Floresta")
-   expect(provider.full_name).to eq "Alta Floresta"
+   expect(build(:provider, first_name: "Alta", last_name: "Floresta").full_name).to eq "Alta Floresta"
   end
 
   it "returns the correct video url format" do
-    provider = Provider.create(first_name: "Alta", last_name: "Floresta", video_url: "https://www.youtube.com/watch?v=GoXv8ojxmGo")
+    provider = create(:provider, video_url: "https://www.youtube.com/watch?v=GoXv8ojxmGo")
     expect(provider.video_url).to eq("http://www.youtube.com/v/GoXv8ojxmGo")
   end
 
   describe "it select deleted and non-deleted Providers" do
     before :each do
-      @not_deleted = Provider.create(first_name: "Alta", last_name: "Floresta", is_deleted: false)
-      @deleted = Provider.create(first_name: "Alta", last_name: "Floresta", is_deleted: true)
+      @not_deleted = create(:provider, is_deleted: false)
+      @deleted = create(:provider, is_deleted: true)
     end
 
-    it "returns just non deleted Providers" do
-      filter = Provider.not_deleted
-      expect(filter).to include(@not_deleted)
-      expect(filter).to_not include(@deleted)
+    context "matching non-deleted" do
+
+      it "returns just non deleted Providers" do
+        filter = Provider.not_deleted
+        expect(filter).to include(@not_deleted)
+        expect(filter).to_not include(@deleted)
+      end
+
     end
 
     it "returns just deleted Providers" do
