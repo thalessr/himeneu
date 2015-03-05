e7ed21a492d47788f0dbae563aed50b19bea0dbc
Thales Ribeiro <thalessr@gmail.com>
2015-03-04 23:56:54 -0600

    Creating a task to generate the sitemap

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,7 @@ set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all
+set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
