14b112469425d4a95080fb0a92a53ae4b0de9a56
Thales Ribeiro <thalessr@gmail.com>
2015-03-06 12:35:09 -0600

    Performance improved - using redis

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -23,7 +23,7 @@ class Interest < ActiveRecord::Base
 
   def self.has_interest?(user, provider)
     if user.is_customer?
-      customer = Customer.find_by(user_id: user.id)
+      customer = Customer.select(:id).find_by(user_id: user.id)
       Interest.exists?(customer_id: customer.id , provider_id: provider.id)
     else
       false
