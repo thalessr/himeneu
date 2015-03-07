3c92f61230874665c46a2968857a0cbb144a0d23
Thales Ribeiro <thalessr@gmail.com>
2015-03-07 17:31:01 -0600

    Chaging states - to be continued :feelsgood:

--- app/helpers/interests_helper.rb
+++ app/helpers/interests_helper.rb
@@ -2,4 +2,16 @@ module InterestsHelper
   def has_interest?(provider)
     Interest.has_interest?(current_user, provider)
   end
+
+  def next_state_action(provider)
+    state = Interest.next_state(current_user, provider)
+    button_to I18n.t(state.to_sym),
+        {
+           :controller => "interests",
+           :action => "change_state",
+           :provider_id => @provider.slug,
+           :user_id => current_user.id,
+           :state => state
+        }, :class => 'btn btn-primary', :method=> :post
+  end
 end
