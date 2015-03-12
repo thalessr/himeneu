eaf318e67ff7ef81159ee7a4d5ef076dfb9915be
Rodrigo Vitulli <rovitulli@gmail.com>
2015-03-12 11:28:56 -0300

    feature_images to profile v1

--- app/controllers/feature_images_controller.rb
+++ app/controllers/feature_images_controller.rb
@@ -2,17 +2,15 @@ require 'pry'
 class FeatureImagesController < ApplicationController
 
   def create
-    binding.pry
     @feature_image = FeatureImage.new(interest_params)
     @feature_image.save
-    binding.pry
     redirect_to provider_path(interest_params["provider_id"])
   end
 
   private
 
   def interest_params
-    params.permit(:image, :provider_id)
+    params.require(:feature_image).permit(:image, :provider_id)
   end
 
 
