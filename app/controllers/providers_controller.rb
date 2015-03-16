9515cef02ffcef28fc066bf71c815092d8bd692a
Vitulli <rovitulli@gmail.com>
2015-03-15 23:29:43 -0300

    Provider Profile V2 and pre merge

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -45,8 +45,7 @@ class ProvidersController < ApplicationController
   end
 
   def show
-    # @provider = Provider.includes(:addresses).friendly.find(params[:id])
-    @provider = Provider.friendly.find(params[:id])
+    @provider = Provider.includes(:addresses).friendly.find(params[:id])
   end
 
   def edit
