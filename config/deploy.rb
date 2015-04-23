b1171f9de667e69300e7cfde3a816e8d2db707db
Thales Ribeiro <thalessr@gmail.com>
2015-04-23 00:51:25 -0500

    Capistrano stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,7 @@ set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rben
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
+set :passenger_restart_with_sudo, false
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
