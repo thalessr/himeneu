4baca0332f7acf428fcbbd2395152ee8b9ef0251
Thales Ribeiro <thalessr@gmail.com>
2015-04-25 08:58:52 -0500

    Sending an estimate email

--- app/helpers/interests_helper.rb
+++ app/helpers/interests_helper.rb
@@ -6,7 +6,7 @@ module InterestsHelper
   def next_state_action(provider)
     state = Interest.next_state(current_user, provider)
     if state == "negotiate".to_sym
-      link_to "Pedir Orçamento", "#estimatesModal", :class=>"btn btn-primary", :id => 'interest'
+      link_to I18n.t(:estimate), "#estimatesModal", :class=>"btn btn-primary", :id => 'estimate'
     else
       button_to I18n.t("state_machine.#{state}"),
       {
