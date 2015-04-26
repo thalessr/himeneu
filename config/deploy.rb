9064bfe196f096716ce85a69382f1d048d89ece7
Thales Ribeiro <thalessr@gmail.com>
2015-04-26 05:44:00 -0500

    From now on, unconfirmed users may still login for 30 days

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,6 +23,7 @@ set :deploy_to, '/home/deployer/apps/himeneu'
 # Default value for :scm is :git
 set :scm, :git
 set :user, "deployer"
+set :deploy_via, :remote_cache
 
 # Default value for :format is :pretty
 set :format, :pretty
