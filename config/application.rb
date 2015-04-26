bebd5084afc32690de59d464a4e57a26809c2ef1
Thales Ribeiro <thalessr@gmail.com>
2015-04-26 02:11:08 -0500

    Fail: connecting to elastic cache

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,14 @@ module Himeneu
     config.i18n.available_locales = [:en, :"pt-BR"]
     config.i18n.default_locale = :"pt-BR"
     config.assets.paths << Rails.root.join("vendor","assets", "bower_components")
-    config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', { expires_in: 90.minutes }
 
+    # if Rails.env.production?
+    #   config.cache_store = :redis_store, 'redis://himeneu-001.8wfbqn.0001.usw2.cache.amazonaws.com/0/cache', { expires_in: 180.minutes }
+    # else
+    #   config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', { expires_in: 90.minutes }
+    # end
+
+    config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', { expires_in: 90.minutes }
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
