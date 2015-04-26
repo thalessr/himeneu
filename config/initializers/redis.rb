23446f3b2d482714ab533a29c355ded5f699b706
Thales Ribeiro <thalessr@gmail.com>
2015-04-26 02:11:08 -0500

    Fail: connecting to elastic cache

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,6 @@
+# if Rails.env.production?
+#   $redis = Redis.new(:host => 'himeneu-001.8wfbqn.0001.usw2.cache.amazonaws.com', :port => 6379)
+# else
+#  $redis = Redis.new(:host => 'localhost', :port => 6379)
+# end
 $redis = Redis.new(:host => 'localhost', :port => 6379)
