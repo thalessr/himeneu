bfc6891a84fe454d57af49b4a5b3bf7f19b9104b
Thales Ribeiro <thalessr@gmail.com>
2015-05-01 03:00:24 -0500

    Providers'form new style

--- app/assets/javascripts/app/app.routes.js
+++ app/assets/javascripts/app/app.routes.js
@@ -1,1 +1,14 @@
+// app.config(['$routeProvider',function($routeProvider) {
 
+// $routeProvider
+//    .when('/provider', {
+//    templateUrl: 'views/providers.html',
+//    controller: 'ProviderCtrl'
+//    })
+
+//    .otherwise({
+//    redirectTo: '/customers'
+//    });
+
+
+// }]);
