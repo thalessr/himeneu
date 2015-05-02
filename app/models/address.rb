4db843147eadcad1489282731fac74790d2ed3ac
Thales Ribeiro <thalessr@gmail.com>
2015-05-01 22:58:49 -0500

    Adding the address fielf into Address model because this field is required by geocoder

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,41 +1,62 @@
 class Address < ActiveRecord::Base
-	has_one  :customer
-	belongs_to :provider
+  has_one  :customer
+  belongs_to :provider
 
-	geocoded_by :full_address
-    # after_validation :geocode_address, :if => :has_changes?
-    after_validation :geocode
+  geocoded_by :full_address
+  after_validation :geocode, if: :has_changes?
 
-    private
-    def full_address
-    	if self.city and self.zipcode
-	    	"#{city},#{zipcode}"
-	    elsif self.city
-	    	"#{city}"
-	    else
-	    	"#{zipcode}"
-	    end
+  reverse_geocoded_by :latitude, :longitude do |obj,results|
+    if geo = results.first
+      obj.city    = geo.city
+      obj.state   = geo.state
+      obj.zipcode = geo.postal_code
+      obj.country = geo.country
     end
+  end
 
-    private
-    def has_changes?
-    	:city_changed? || :zipcode_changed?
+  after_validation :reverse_geocode, if: :has_changes?
+
+
+  def address
+    if self.city and self.zipcode
+      "#{city},#{zipcode}"
+    elsif self.city
+      "#{city}"
+    else
+      "#{zipcode}"
     end
+  end
 
-    private
-    def geocode_address
-  		GeocodeWorker.perform_async(self.id)
-	  end
+  private
+  def full_address
+    if self.city and self.zipcode
+      "#{city},#{zipcode}"
+    elsif self.city
+      "#{city}"
+    else
+      "#{zipcode}"
+    end
+  end
 
-	class GeocodeWorker
-	    include Sidekiq::Worker
-	    sidekiq_options :queue => :default
+  private
+  def has_changes?
+    :city_changed? || :zipcode_changed?
+  end
 
-	    def perform(id)
-	      address = Address.find(id)
-	      address.geocode
-	      address.save!
-	    end
-	end
+  private
+  def geocode_address
+    GeocodeWorker.perform_async(self.id)
+  end
+
+  class GeocodeWorker
+    include Sidekiq::Worker
+    sidekiq_options :queue => :default
+
+    def perform(id)
+      address = Address.find(id)
+      address.geocode
+      address.save!
+    end
+  end
 
 end
