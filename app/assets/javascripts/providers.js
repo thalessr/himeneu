202f8cc2a722258c875b00b9a9c935ffe8afc929
Thales Ribeiro <thalessr@gmail.com>
2015-05-04 03:51:50 -0500

    Gallery(Feature Image)

--- app/assets/javascripts/providers.js
+++ app/assets/javascripts/providers.js
@@ -10,80 +10,80 @@ function add_fields(link, association, content) {
   $("#addresses").append(content.replace(regexp, new_id));
 }
 
-function showScore(){
-   $('div#provider_rating').raty({
-      readOnly: function() {
-        return true;
-      },
-      score: function() {
-        return $(this).attr('data-score');
-      },
-  });
-}
+// function showScore(){
+//    $('div#provider_rating').raty({
+//       readOnly: function() {
+//         return true;
+//       },
+//       score: function() {
+//         return $(this).attr('data-score');
+//       },
+//   });
+// }
 
-$(document).ready(function(){
-  $.fn.raty.defaults.path = "/assets";
-  $.fn.raty.defaults.half_show = true;
+// $(document).ready(function(){
+//   $.fn.raty.defaults.path = "/assets";
+//   $.fn.raty.defaults.half_show = true;
 
 
 
-$( "#estimate" ).click(function() {
-  $('#estimatesModal').modal('toggle');
-});
+// $( "#estimate" ).click(function() {
+//   $('#estimatesModal').modal('toggle');
+// });
 
-  $('#provider_form').bind('ajax:success', function(evt, data, status, xhr) {
-  	$('#error_msg').remove();
-     $('.bottom-right').notify({
-        message: { text: xhr.getResponseHeader("X-Flash-Notice") },
-        type: 'success'
-      }).show();
+//   $('#provider_form').bind('ajax:success', function(evt, data, status, xhr) {
+//   	$('#error_msg').remove();
+//      $('.bottom-right').notify({
+//         message: { text: xhr.getResponseHeader("X-Flash-Notice") },
+//         type: 'success'
+//       }).show();
 
-      if (null !== data.slug) {
-        window.location.href = "/providers/"+data.slug;
-      }
+//       if (null !== data.slug) {
+//         window.location.href = "/providers/"+data.slug;
+//       }
 
-   });
-   $('#provider_form').bind('ajax:error', function(data, xhr, status) {
+//    });
+//    $('#provider_form').bind('ajax:error', function(data, xhr, status) {
 
-      var errors, errorText;
+//       var errors, errorText;
 
-      errors = $.parseJSON(xhr.responseText);
+//       errors = $.parseJSON(xhr.responseText);
 
-      for ( var error in errors ) {
-        errorText = "";
-        if (typeof error !== "undefined" && typeof errors[error] !== "undefined"){
-           errorText += error + ': ' + errors[error];
-            var field = error.replace(':', "");
-            $("#"+field).closest('div').addClass('has-error');
-        }
+//       for ( var error in errors ) {
+//         errorText = "";
+//         if (typeof error !== "undefined" && typeof errors[error] !== "undefined"){
+//            errorText += error + ': ' + errors[error];
+//             var field = error.replace(':', "");
+//             $("#"+field).closest('div').addClass('has-error');
+//         }
 
-      }
+//       }
 
-     $('.bottom-right').notify({
-        message: { text: errorText},
-        type: 'danger'
-      }).show();
+//      $('.bottom-right').notify({
+//         message: { text: errorText},
+//         type: 'danger'
+//       }).show();
 
-   });
+//    });
 
-  $('#rate').raty();
-  showScore();
+//   $('#rate').raty();
+//   showScore();
 
-});
+// });
 
-$(window).bind("load", function(){
-  $.fn.raty.defaults.path = "/assets";
-  $.fn.raty.defaults.half_show = true;
-  $('div#score').raty({
-      readOnly: function() {
-        return true;
-      },
-      score: function() {
-        return $(this).attr('data-score');
-      }
-  });
+// $(window).bind("load", function(){
+//   $.fn.raty.defaults.path = "/assets";
+//   $.fn.raty.defaults.half_show = true;
+//   $('div#score').raty({
+//       readOnly: function() {
+//         return true;
+//       },
+//       score: function() {
+//         return $(this).attr('data-score');
+//       }
+//   });
 
-});
+// });
 
 
 
