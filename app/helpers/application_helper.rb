c73c6ba41caf76680a868061ca943a8e1cb52060
Thales Ribeiro <thalessr@gmail.com>
2015-05-25 00:02:47 -0300

    Chat testing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,19 +16,45 @@ module ApplicationHelper
     name = object.class.to_s.underscore
     if object.is_deleted?
       link_to send("recover_#{name.pluralize}_path",id: "#{object.slug}") ,class: "btn btn-info" do
-         content_tag(:span, I18n.t('links.activate'), class: "", 'data-original-title'.to_sym => t('links.activate'), "data-toggle".to_sym => "tooltip" )
+        content_tag(:span, I18n.t('links.activate'), class: "", 'data-original-title'.to_sym => t('links.activate'), "data-toggle".to_sym => "tooltip" )
       end
     elsif !object.slug.blank?
       link_to send("#{name}_path",object.slug), method: :delete, data: { confirm: t('links.confirm') }  ,class: "btn btn-danger" do
-         content_tag(:span, I18n.t('links.destroy'), class: "", 'data-original-title'.to_sym => t('links.destroy'), "data-toggle".to_sym => "tooltip" )
+        content_tag(:span, I18n.t('links.destroy'), class: "", 'data-original-title'.to_sym => t('links.destroy'), "data-toggle".to_sym => "tooltip" )
       end
     end
   end
 
-def resource_class
-  devise_mapping.to
-end
+  def resource_class
+    devise_mapping.to
+  end
+
+
+  #
+  # For  provider or supplier account, create a "Chat" menu on header with a link to:
+  # http://chat.himeneu.com/SUPPLIER-SLUG
+  # ie:
+  # http://chat.himeneu.com/himeneu
+  #
+  # For Customer account, in the suppliers list, create a button "Chat" for each supplier with a link to:
+  # http://chat.himeneu.com/SUPPLIER-SLUG/CUSTOMER-SLUG
+  # ie:
+  # http://chat.himeneu.com/himeneu/thales
+  #
+  #
+  def chat_url(provider = nil)
+    base_url = "http://chat.himeneu.com/"
+
+    unless provider.blank?
+      base_url += provider
+      if current_user.is_customer?
+        base_url += "/#{current_user.customer.slug}"
+      end
+      link_to base_url do
+        content_tag(:span, "",class: "fa fa-weixin", 'data-original-title'.to_sym => "Chat", "data-toggle".to_sym => "tooltip", "data-placement".to_sym => "top")
+      end
+    end
+
+  end
 
 end
-
-
