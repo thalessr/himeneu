5185a7d70a03f7ae1b46ababa210ac3b6f821c6c
Thales Ribeiro <thalessr@gmail.com>
2015-10-28 14:07:42 -0200

    Updating the sintaxe from Gibbon::API.api_key to  Gibbon::Request.api_key

--- config/initializers/mailchimp.rb
+++ config/initializers/mailchimp.rb
@@ -1,3 +1,3 @@
-Gibbon::API.api_key = "f77800d9b01ca32cb9dd7c1c7712ed08-us10"
-Gibbon::API.timeout = 30
-Gibbon::API.throws_exceptions = false
+Gibbon::Request.api_key = "f77800d9b01ca32cb9dd7c1c7712ed08-us10"
+Gibbon::Request.timeout = 30
+Gibbon::Request.throws_exceptions = false
